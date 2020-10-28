4a4a5b36433ef21ce692bd357ba343df2713bf17
Evandrolds <evandro.lds.ds@outlook.com>
2020-10-27 23:00:58 -0300

    FindAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,16 +14,24 @@ import model.enttities.Department;
 public class Program {
 
     public static void main(String[] args) {
-         SellerDao sellerDao = Daofactory.createSellerDao();
-         System.out.println("===  Teste 1: Seller FindById  === ");
+        SellerDao sellerDao = Daofactory.createSellerDao();
+
+        System.out.println("===  Teste 1: Seller FindById  === ");
         Seller seller = sellerDao.findById(4);
         System.out.println(seller);
-        
+
         System.out.println("\n === Teste 2: Department FindByDepartment === ");
         Department dep = new Department(2, null);
         List<Seller> list = sellerDao.findByDemarment(dep);
         for (Seller seller1 : list) {
             System.out.println(seller1);
+
+        }
+
+        System.out.println("\n === Teste 3: Department FindAll === ");
+        list = sellerDao.findAll();
+        for (Seller seller2 : list) {
+            System.out.println(seller2);
         }
     }
 }
--- src/model/dao/Impl/SellerDaoJDBC.java
+++ src/model/dao/Impl/SellerDaoJDBC.java
@@ -8,6 +8,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -23,7 +24,8 @@ import model.enttities.Seller;
  * @author Evandro
  */
 public class SellerDaoJDBC implements SellerDao {
-public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     private Connection con;
 
     public SellerDaoJDBC(Connection con) { // para forçar a injeção de dependedncias
@@ -37,28 +39,27 @@ public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     public void insert(Seller obj) {
         PreparedStatement pst = null;
-        
+
         try {
             pst = con.prepareStatement("Insert into seller set (Name,Email,BirthDate,BaseSalary,DepartmentId");
             pst.setString(1, "Sergio");
-            pst.setString(2,"sergion@hormail.com");
+            pst.setString(2, "sergion@hormail.com");
             pst.setDate(3, new java.sql.Date(sdf.parse("22/08/2000").getTime()));
-            pst.setDouble(4,2550);
-            pst.setInt(5,4);
-            
+            pst.setDouble(4, 2550);
+            pst.setInt(5, 4);
+
             pst.executeQuery();
-            
+
             System.out.println("Dados Inserido com sucesso!");
         } catch (SQLException ex) {
-            System.out.println("Erro na Inserção de dados!"+ ex.getMessage());
+            System.out.println("Erro na Inserção de dados!" + ex.getMessage());
         } catch (ParseException ex) {
             System.out.println("Erro na Dada " + ex.getMessage());
-    }
-        finally{
+        } finally {
             bd.BD.coloseStatement(pst);
             bd.BD.closeConnection();
         }
-        
+
     }
 
     @Override
@@ -77,11 +78,11 @@ public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         PreparedStatement pst = null;
         ResultSet rs = null;
         try {
-           
-            pst = con.prepareStatement("SELECT seller.*,department.Name as DepName " +
-                                       "FROM seller INNER JOIN department " +
-                                       "ON seller.DepartmentId = department.Id " +
-                                       "WHERE seller.Id = ?");
+
+            pst = con.prepareStatement("SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
             pst.setInt(1, id);
             rs = pst.executeQuery();
             if (rs.next()) {
@@ -97,8 +98,6 @@ public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
             bd.BD.coloseResulset(rs);
             bd.BD.coloseStatement(pst);
-            
-            
 
         }
 
@@ -113,6 +112,31 @@ public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     public List<Seller> findAll() {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = con.prepareStatement("SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "ORDER BY Name");
+            rs = pst.executeQuery();
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new Hashtable<>();
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+
+                }
+                Seller seller = instantiateSeller(rs, dep);
+                list.add(seller);
+
+            }
+            return list;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
         return null;
     }
 
@@ -131,31 +155,31 @@ public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     public List<Seller> findByDemarment(Department department) {
         PreparedStatement pst = null;
         ResultSet rs = null;
-        try{
-        pst = con.prepareStatement("SELECT seller.*,department.Name as DepName " +
-                                       "FROM seller INNER JOIN department " +
-                                       "ON seller.DepartmentId = department.Id " +
-                                       "WHERE DepartmentId = ? " + "ORDER BY Name");
-        pst.setInt(1, department.getId());
-         rs = pst.executeQuery();
-         List<Seller> list = new ArrayList<>();
-            Map<Integer,Department> map = new HashMap<>();
-       while(rs.next()){
-           Department dep = map.get(rs.getInt("DepartmentId"));
-          if(dep == null){
-               dep = instantiateDepartment(rs);
-               map.put(rs.getInt("DepartmentId"), dep);
-           }
-            
-            Seller seller = instantiateSeller(rs, dep);
-            list.add(seller);
-        }
-        return list;
-        }catch(SQLException ex){
-            System.out.println(" Error na quuery:" +ex.getMessage());
+        try {
+            pst = con.prepareStatement("SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE DepartmentId = ? " + "ORDER BY Name");
+            pst.setInt(1, department.getId());
+            rs = pst.executeQuery();
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = instantiateSeller(rs, dep);
+                list.add(seller);
+            }
+            return list;
+        } catch (SQLException ex) {
+            System.out.println(" Error na quuery:" + ex.getMessage());
         }
         return null;
-        
+
     }
 
 }
